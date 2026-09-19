6880cbb9b69fba098e0b8b7a28964316078f22fd
luminous479 <aklemasultanakhi@gmail.com>
2026-09-19 19:11:45 +0600

    fix: correct product update and request validation

--- main.go
+++ main.go
@@ -8,30 +8,30 @@ import (
 )
 
 type Product struct {
-	Id       int  `json:"id"`
-	Name     string `json:"name"`
-	SKU      string `json:"sku"`
+	ID       int     `json:"id"`
+	Name     string  `json:"name"`
+	SKU      string  `json:"sku"`
 	Price    float64 `json:"price"`
-	Quantity int  `json: "quantity"`
+	Quantity int     `json:"quantity"`
 }
 
 var products = []Product{
 	{
-		Id:       1,
+		ID:       1,
 		Name:     "Logitech Keyboard",
 		SKU:      "KB-001",
 		Price:    45,
 		Quantity: 50,
 	},
 	{
-		Id:       2,
+		ID:       2,
 		Name:     "Dell Monitor",
 		SKU:      "MN-001",
 		Price:    220,
 		Quantity: 20,
 	},
 	{
-		Id:       3,
+		ID:       3,
 		Name:     "HP Mouse",
 		SKU:      "MS-001",
 		Price:    25,
@@ -40,12 +40,10 @@ var products = []Product{
 }
 
 func main() {
-
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /products", getProducts)
 	mux.HandleFunc("POST /products", createProduct)
-
 	mux.HandleFunc("GET /products/{id}", getProduct)
 	mux.HandleFunc("PUT /products/{id}", updateProduct)
 	mux.HandleFunc("DELETE /products/{id}", deleteProduct)
@@ -59,33 +57,30 @@ func main() {
 }
 
 func getProducts(w http.ResponseWriter, r *http.Request) {
-
 	fmt.Fprintln(w, "GET: All products")
 
 	for _, product := range products {
-
 		fmt.Fprintf(
 			w,
 			"ID: %d | Name: %s | SKU: %s | Price: $%.2f | Quantity: %d\n",
-			product.Id,
+			product.ID,
 			product.Name,
 			product.SKU,
 			product.Price,
 			product.Quantity,
 		)
-
 	}
 }
 
 func createProduct(w http.ResponseWriter, r *http.Request) {
-
 	var product Product
 
 	err := json.NewDecoder(r.Body).Decode(&product)
-
 	if err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadGateway)
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
 	}
+
 	if product.Name == "" {
 		http.Error(w, "Product name is required", http.StatusBadRequest)
 		return
@@ -105,33 +100,32 @@ func createProduct(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Product quantity cannot be negative", http.StatusBadRequest)
 		return
 	}
-	product.Id = len(products) + 1
+
+	product.ID = len(products) + 1
 
 	products = append(products, product)
-    w.Header().Set("Content-Type","application/json")
-	w.Header().Set("Location",fmt.Sprintf("/products/%d",product.Id))
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(product)
 
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Location", fmt.Sprintf("/products/%d", product.ID))
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(product)
 }
 
 func getProduct(w http.ResponseWriter, r *http.Request) {
-
 	id, err := strconv.Atoi(r.PathValue("id"))
 
 	if err != nil {
-		http.Error(w, "Invalid Product Id ", http.StatusBadRequest)
+		http.Error(w, "Invalid product ID", http.StatusBadRequest)
 		return
 	}
 
 	for _, product := range products {
-
-		if product.Id == id {
-
+		if product.ID == id {
 			fmt.Fprintf(
 				w,
 				"ID: %d | Name: %s | SKU: %s | Price: $%.2f | Quantity: %d\n",
-				product.Id,
+				product.ID,
 				product.Name,
 				product.SKU,
 				product.Price,
@@ -139,57 +133,73 @@ func getProduct(w http.ResponseWriter, r *http.Request) {
 			)
 			return
 		}
-
 	}
 
 	http.Error(w, "Product not found", http.StatusNotFound)
-
 }
 
 func updateProduct(w http.ResponseWriter, r *http.Request) {
-
-	
-
 	id, err := strconv.Atoi(r.PathValue("id"))
-      if err != nil{
-				http.Error(w, "Invalid Request Body ", http.StatusBadRequest)
-	  }
-	  var updateProduct Product
-     err = json.NewDecoder(r.Body).Decode(&updateProduct)
 
 	if err != nil {
-		http.Error(w, "Invalid Request Body ", http.StatusBadRequest)
+		http.Error(w, "Invalid product ID", http.StatusBadRequest)
 		return
 	}
 
+	var updatedProduct Product
 
-	for i, product := range products{
-
-		if product.Id == id {
-			updateProduct.Id = id
-          products[i] = updateProduct
-		}
-		w.Header().Set("Content-Type","application/json")
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(updateProduct)
-		return 
+	err = json.NewDecoder(r.Body).Decode(&updatedProduct)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
 	}
 
-	http.Error(w,"Product Not Found",http.StatusNotFound)
+	if updatedProduct.Name == "" {
+		http.Error(w, "Product name is required", http.StatusBadRequest)
+		return
+	}
 
+	if updatedProduct.SKU == "" {
+		http.Error(w, "Product SKU is required", http.StatusBadRequest)
+		return
+	}
 
+	if updatedProduct.Price <= 0 {
+		http.Error(w, "Product price must be greater than 0", http.StatusBadRequest)
+		return
+	}
+
+	if updatedProduct.Quantity < 0 {
+		http.Error(w, "Product quantity cannot be negative", http.StatusBadRequest)
+		return
+	}
+
+	for i, product := range products {
+		if product.ID == id {
+			updatedProduct.ID = id
+			products[i] = updatedProduct
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+
+			json.NewEncoder(w).Encode(updatedProduct)
+			return
+		}
+	}
+
+	http.Error(w, "Product not found", http.StatusNotFound)
 }
 
 func deleteProduct(w http.ResponseWriter, r *http.Request) {
-	idString := r.PathValue("id")
-	id, err := strconv.Atoi(idString)
+	id, err := strconv.Atoi(r.PathValue("id"))
+
 	if err != nil {
 		http.Error(w, "Invalid product ID", http.StatusBadRequest)
 		return
 	}
 
 	for i, product := range products {
-		if product.Id == id {
+		if product.ID == id {
 			products = append(products[:i], products[i+1:]...)
 
 			w.WriteHeader(http.StatusNoContent)
