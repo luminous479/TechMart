407adf16e159c189a1d7e7baef1d540f12acfd37
luminous479 <aklemasultanakhi@gmail.com>
2026-09-19 15:10:49 +0600

    feat: add RESTful product routes

--- main.go
+++ main.go
@@ -3,33 +3,54 @@ package main
 import (
 	"fmt"
 	"net/http"
-
 )
 
 func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /products", productHandler)
+	mux.HandleFunc("GET /products", getProducts)
+	mux.HandleFunc("POST /products", createProduct)
 
-	mux.HandleFunc("GET /products/{id}", productByIdHandler)
+	mux.HandleFunc("GET /products/{id}", getProduct)
+	mux.HandleFunc("PUT /products/{id}", updateProduct)
+	mux.HandleFunc("DELETE /products/{id}", deleteProduct)
 
-	fmt.Println("server is running at 8080")
+	fmt.Println("server is running at http://localhost:8080")
 
 	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
-
 }
 
-func productHandler(w http.ResponseWriter, r *http.Request) {
+func getProducts(w http.ResponseWriter, r *http.Request) {
 
-
-	fmt.Fprintln(w,"View All Products")
+	fmt.Fprintln(w, "GET: All products")
 }
 
-func productByIdHandler(w http.ResponseWriter, r *http.Request){
+func createProduct(w http.ResponseWriter, r *http.Request) {
 
-	fmt.Fprintln(w, "Product ID : ", r.PathValue("id"))
+	fmt.Fprintln(w, "POST: Create a product")
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+
+	id := r.PathValue("id")
+
+	fmt.Fprintln(w, "GET: Product ID:", id)
+}
+
+func updateProduct(w http.ResponseWriter, r *http.Request) {
+
+	id := r.PathValue("id")
+
+	fmt.Fprintln(w, "PUT: Update product ID:", id)
+}
+
+func deleteProduct(w http.ResponseWriter, r *http.Request) {
+
+	id := r.PathValue("id")
+
+	fmt.Fprintln(w, "DELETE: Delete product ID:", id)
 }
